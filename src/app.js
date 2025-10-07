25f1e383b3ae9e7cf380ce40cbf8d5039e9279ae
Deepak Yadav <deepak226713@gmail.com>
2025-10-07 23:30:20 +0530

    working with routes

--- src/app.js
+++ src/app.js
@@ -2,22 +2,10 @@ const express = require("express");
 
 const app = express();
 
-
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Radhey", lastName: "Shyam" });
-});
-
-app.post("/user", (req, res) => {
-    // Logic to save database in DB
-    res.send("Data Successfully saved to the dataase.");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("User Deleted Successfully.");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello, from the Server hello.! /test");
+app.get("/user/:userId/:name/:password", (req, res) => {
+  // const userId = req.params.id;
+  console.log(JSON.parse(JSON.stringify(req.params)));
+  res.send({firstname: "Radhey", lastName: "Shyam" });
 });
 
 app.listen(7777, () => {
