2ae5a6527240d93b2a7409e56d00934fd63c6bb2
Deepak Yadav <deepak226713@gmail.com>
2025-10-14 01:44:53 +0530

    wildcard-error-handlers

--- src/app.js
+++ src/app.js
@@ -1,30 +1,27 @@
 const express = require("express");
-const { adminAuth, userAuth, studentAuth } = require("./middlewares/auth");
 const app = express();
 
-// Handle Auth Middleware for all Get Post , ... request
-app.use("/admin", adminAuth);
-
-app.post("/user/login", (req, res) => {
-  res.send("User logged in successfully.");
-});
-
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("User Get Data Send Successfullly");
-});
-
-app.get("/student", studentAuth, (req, res) => {
-  res.send("Welcome Students");
+app.use("/", (err, req, res, next) => {
+  if(err) {
+    res.status(500).send("Something went wrong..!")
+  }
 })
- 
-app.get("/admin/getAllData", (req, res) => {
-  // Logic of fetching all data.
-  res.send("Get Data Sent");
+
+app.get("/getUserData", (req, res) => {
+  try {
+throw new Error("galat baat hai..");
+  res.send("USer Data Sent.");
+  } catch(err) {
+    res.status(500).send("Some error arises contact your support team..!")
+  }
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("USer Deleted.");
+app.use("/", (err, req, res, next) => {
+  if(err) {
+    res.status(500).send("Something went wrong..!")
+  }
 });
+
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777....");
